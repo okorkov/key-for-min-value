0ea6876776f729a19f84ae9e734f9caeb035984b
okorkov <alex.okorkov@gmail.com>
2020-11-11 15:27:27 -0800

    commit tree 9fb113b9f34edb9a3ad443a1747de5c9056619d4

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash.empty? 
+      return nil
+    end
+    values = Array.new
+    name_hash.collect do |key, value|
+        values.push(value)
+    end
+    smallest = values[0]
+    values.each do |value|
+      smallest = value if value < smallest
+    end
+    name_hash.each do |key, value|
+      return key if value == smallest
+    end
 end
